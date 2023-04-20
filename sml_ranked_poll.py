2286003aa58e5fbac35098956047fdb8349f1464
CT Hutt <bookhutt@gmail.com>
2023-04-19 21:34:43 -0600

    Update sml_ranked_poll.py

--- sml_ranked_poll.py
+++ sml_ranked_poll.py
@@ -1,32 +1,18 @@
 import pandas as pd
 
-# Load the CSV file into a pandas dataframe
-df = pd.read_csv('vote.csv')
+# Read the CSV file into a pandas dataframe
+df = pd.read_csv('vote_totals.csv')
 
-# Create a dictionary to store the vote counts
-vote_counts = {}
+# Set the candidate column as the index
+df.set_index('candidate', inplace=True)
 
-# Loop through each row in the dataframe
-for index, row in df.iterrows():
-    # Get the candidate's name and first and second choice
-    candidate = row['candidates']
-    first_choice = row['first_choice']
-    second_choice = row['second_choice']
+# Calculate second place votes by dividing the second_choice_total column by 2
+second_place_votes = df['second_choice_total'] / 2
 
-    # If the candidate's first choice is not already in the vote count dictionary, add it with a count of 0
-    if first_choice not in vote_counts:
-        vote_counts[first_choice] = 0
+# Combine first and second place votes
+total_votes = df['first_choice_total'].add(second_place_votes, fill_value=0)
 
-    # If the candidate's second choice is not already in the vote count dictionary, add it with a count of 0
-    if second_choice not in vote_counts:
-        vote_counts[second_choice] = 0
+# Determine the winner
+winner = total_votes.idxmax()
 
-    # Increment the vote count for the candidate's first choice
-    vote_counts[first_choice] += 1
-
-    # If the candidate's first choice did not win, increment the vote count for their second choice
-    if vote_counts[first_choice] < vote_counts[max(vote_counts, key=vote_counts.get)]:
-        vote_counts[second_choice] += 1
-
-# Print out the final vote counts
-print(vote_counts)
+print(f"The winner is {winner} with {total_votes[winner]} votes!")
